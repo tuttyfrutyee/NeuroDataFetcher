79ce5cf85c9d942f9c8f0c1b2e9036eef620a528
anakin <atakan.stanford.edu>
2025-01-06 04:42:31 +0000

    trying to fix import error on conda

---
+++ neurodatafetcher/__init__.py
@@ -0,0 +1,6 @@
+# Import and expose your main classes/functions
+from .your_main_module import YourMainClass
+# Add any other imports you want to make available
+
+# Optionally specify __all__
+__all__ = ['YourMainClass'] 
--- setup.py
+++ setup.py
@@ -1,28 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="neuro_data_fetcher",  # Replace with your desired package name
-    version="0.1.0",
-    author="Atakan(aka Anakin)",
-    author_email="atakan@stanford.edu",
-    description="A Python package for fetching and processing neuroscience data",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/tuttyfrutyee/NeuroDataFetcher.git",  # Replace with your repository URL
+    name="NeuroDataFetcher",
+    version="0.1",
     packages=find_packages(),
-    package_dir={"": "src"},  # Tell setuptools where to find the packages
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
     install_requires=[
-        # "h5py>=3.12.1",
-        # "numpy>=2.2.1",
-        # "pandas>=2.2.3",
-        # "pynwb>=2.8.3",
-        # "scipy>=1.14.1",
-        # "setuptools>=59.6.0",
+        # list your dependencies here
     ],
 )
